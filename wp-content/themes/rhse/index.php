7949e07705b4478cdaff0a866bdd423ebaaedaf6
Mohammed Bensaad <bensaadmucret@gmail.com>
2020-06-30 13:56:11 +0200

    first commit

---
+++ wp-content/themes/rhse/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ */
+
+get_header(); ?>
+
+<div id="primary">
+<div id="content" role="main">
+			
+<br/>
+<strong>Édition spéciale pénibilité</strong> : la 6<sup>ème</sup> édition des Rencontres HSE se déroulera le jeudi 12 Mars 2015 à l'hôtel Pullman
+ Montparnasse - Paris de 8h30 à 16H45 <br /><br />
+            
+<table>
+<tbody>
+<tr>
+<td><a href="https://www.rencontreshse.fr/wp-content/uploads/2015/01/HSE_Présentation-RHSE-2015.pdf" border="0" target="_blank"><strong>TÉLÉCHARGER LE DOSSIER DE PRÉSENTATION
+</strong></a></td>
+</tr>
+</tbody>
+</table>
+<br/>
+
+
+<a href="https://www.rencontreshse.fr/retour-6eme-edition/"><img src="https://www.rencontreshse.fr/wp-content/uploads/2015/03/retourimage-01.jpg" /></a>
+
+<br />
+
+
+</div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
